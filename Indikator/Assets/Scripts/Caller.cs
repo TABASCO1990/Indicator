065dc8f0b4275864a10463d80abc6f59e04c34b8
TABASCO <fifabox@gmail.com>
2024-02-22 19:25:32 +0500

    Add namespace scripts

--- Indikator/Assets/Scripts/Caller.cs
+++ Indikator/Assets/Scripts/Caller.cs
@@ -2,38 +2,41 @@ using System;
 using UnityEngine;
 using TMPro;
 
-public class Caller : MonoBehaviour
+namespace Indicator
 {
-    [SerializeField] private TMP_Text _title;
-    [SerializeField] private string _textStart;
-    [SerializeField] private string _textStop;
-
-    private bool _isPressed = true;
-
-    public event Action PressedButton;
-
-    public bool IsPressed => _isPressed;
-
-    private void Start()
+    public class Caller : MonoBehaviour
     {
-        _title.text = _textStart;
-    }
+        [SerializeField] private TMP_Text _title;
+        [SerializeField] private string _textStart;
+        [SerializeField] private string _textStop;
 
-    public void ToggleText()
-    {
-        GetText();
-        PressedButton?.Invoke();
-    }
+        private bool _isPressed = true;
 
-    private string GetText()
-    {
-        if (_title.text == _textStart)
+        public event Action PressedButton;
+
+        public bool IsPressed => _isPressed;
+
+        private void Start()
         {
-            _isPressed = true;
-            return _title.text = _textStop;
+            _title.text = _textStart;
         }
-        else
-            _isPressed = false;
-        return _title.text = _textStart;
+
+        public void ToggleText()
+        {
+            GetText();
+            PressedButton?.Invoke();
+        }
+
+        private string GetText()
+        {
+            if (_title.text == _textStart)
+            {
+                _isPressed = true;
+                return _title.text = _textStop;
+            }
+            else
+                _isPressed = false;
+            return _title.text = _textStart;
+        }
     }
 }
--- Indikator/Assets/Scripts/IndicatorController.cs
+++ Indikator/Assets/Scripts/IndicatorController.cs
@@ -3,117 +3,120 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-[RequireComponent(typeof(Caller))]
-public class IndicatorController : MonoBehaviour
+namespace Indicator
 {
-    private const int MinValue = 10;
-    private const int MaxValue = 21;
-    private const int EndValue = 0;
-
-    [SerializeField] private TMP_Text _textIndicatorFirst;
-    [SerializeField] private TMP_Text _textIndicatorSecond;
-
-    [SerializeField] private Image _colorIndicatorFirst;
-    [SerializeField] private Image _colorIndicatorSecond;
-
-    private Caller _call;
-
-    private IEnumerator _firstIndicator;
-    private IEnumerator _secondIndicator;
-
-    private int _timer;
-
-    private void Awake()
+    [RequireComponent(typeof(Caller))]
+    public class IndicatorController : MonoBehaviour
     {
-        _call = GetComponent<Caller>();
-        SetDefaultValues();
-    }
+        private const int MinValue = 10;
+        private const int MaxValue = 21;
+        private const int EndValue = 0;
 
-    private void OnEnable()
-    {
-        _call.PressedButton += OnPressedButton;
-    }
+        [SerializeField] private TMP_Text _textIndicatorFirst;
+        [SerializeField] private TMP_Text _textIndicatorSecond;
 
-    private void OnDisable()
-    {
-        _call.PressedButton -= OnPressedButton;
-    }
+        [SerializeField] private Image _colorIndicatorFirst;
+        [SerializeField] private Image _colorIndicatorSecond;
 
-    private void OnPressedButton()
-    {
-        if (_call.IsPressed)
+        private Caller _call;
+
+        private IEnumerator _firstIndicator;
+        private IEnumerator _secondIndicator;
+
+        private int _timer;
+
+        private void Awake()
         {
-            _firstIndicator = InitiateFistIndicator();
-            _secondIndicator = InitiateSecondIndicator();
-
-            _colorIndicatorFirst.color = Color.red;
-            _colorIndicatorSecond.color = Color.red;
-
-            _firstIndicator.MoveNext();
-            StartCoroutine(_secondIndicator);
-        }
-        else
-        {
+            _call = GetComponent<Caller>();
             SetDefaultValues();
-            StopAllCoroutines();
         }
-    }
 
-    private void InitializeData(Image color, TMP_Text text)
-    {
-        _timer = Random.Range(MinValue, MaxValue);
-        color.color = Color.green;
-        text.text = _timer.ToString();
-    }
-
-    private void SetDefaultValues()
-    {
-        _colorIndicatorFirst.color = Color.white;
-        _colorIndicatorSecond.color = Color.white;
-        _textIndicatorFirst.text = "0";
-        _textIndicatorSecond.text = "0";
-    }
-
-    private IEnumerator InitiateFistIndicator()
-    {
-        InitializeData(_colorIndicatorFirst, _textIndicatorFirst);
-
-        while (true)
+        private void OnEnable()
         {
-            _timer--;
-            _textIndicatorFirst.text = _timer.ToString();
+            _call.PressedButton += OnPressedButton;
+        }
 
-            yield return new WaitForSeconds(1);
+        private void OnDisable()
+        {
+            _call.PressedButton -= OnPressedButton;
+        }
 
-            if (_timer == EndValue)
+        private void OnPressedButton()
+        {
+            if (_call.IsPressed)
             {
+                _firstIndicator = InitiateFistIndicator();
+                _secondIndicator = InitiateSecondIndicator();
+
                 _colorIndicatorFirst.color = Color.red;
-                break;
-            }
-        }
-
-        yield return StartCoroutine(InitiateSecondIndicator());
-    }
-
-    private IEnumerator InitiateSecondIndicator()
-    {
-        yield return _firstIndicator;
-
-        InitializeData(_colorIndicatorSecond, _textIndicatorSecond);
-
-        while (true)
-        {
-            _timer--;
-            _textIndicatorSecond.text = _timer.ToString();
-            yield return new WaitForSeconds(1);
-
-            if (_timer == EndValue)
-            {
                 _colorIndicatorSecond.color = Color.red;
-                break;
+
+                _firstIndicator.MoveNext();
+                StartCoroutine(_secondIndicator);
+            }
+            else
+            {
+                SetDefaultValues();
+                StopAllCoroutines();
             }
         }
 
-        yield return StartCoroutine(InitiateFistIndicator());
+        private void InitializeData(Image color, TMP_Text text)
+        {
+            _timer = Random.Range(MinValue, MaxValue);
+            color.color = Color.green;
+            text.text = _timer.ToString();
+        }
+
+        private void SetDefaultValues()
+        {
+            _colorIndicatorFirst.color = Color.white;
+            _colorIndicatorSecond.color = Color.white;
+            _textIndicatorFirst.text = "0";
+            _textIndicatorSecond.text = "0";
+        }
+
+        private IEnumerator InitiateFistIndicator()
+        {
+            InitializeData(_colorIndicatorFirst, _textIndicatorFirst);
+
+            while (true)
+            {
+                _timer--;
+                _textIndicatorFirst.text = _timer.ToString();
+
+                yield return new WaitForSeconds(1);
+
+                if (_timer == EndValue)
+                {
+                    _colorIndicatorFirst.color = Color.red;
+                    break;
+                }
+            }
+
+            yield return StartCoroutine(InitiateSecondIndicator());
+        }
+
+        private IEnumerator InitiateSecondIndicator()
+        {
+            yield return _firstIndicator;
+
+            InitializeData(_colorIndicatorSecond, _textIndicatorSecond);
+
+            while (true)
+            {
+                _timer--;
+                _textIndicatorSecond.text = _timer.ToString();
+                yield return new WaitForSeconds(1);
+
+                if (_timer == EndValue)
+                {
+                    _colorIndicatorSecond.color = Color.red;
+                    break;
+                }
+            }
+
+            yield return StartCoroutine(InitiateFistIndicator());
+        }
     }
 }
